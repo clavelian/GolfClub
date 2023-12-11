e913c20dcf44e6d69761562cce622cb92024b85e
Clavelian <46989875+clavelian@users.noreply.github.com>
2023-12-11 15:44:41 +0800

    shirt

---
+++ shirt.js
@@ -0,0 +1,20 @@
+/*Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+ */
+
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+};
+
+// Only change code below this line
+const hatValue = testObj; // Change this line
+const shirtValue = testObj; // Change this line
+
+/*hatValue should be a string
+Waiting:The value of hatValue should be the string ballcap
+Waiting:shirtValue should be a string
+Waiting:The value of shirtValue should be the string jersey
+Waiting:You should use dot notation twice
+*/
